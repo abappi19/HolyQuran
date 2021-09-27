ec14dd912f8ed58760f07fe826f3b2559c6bd346
hbappi <contact.efortshub@gmail.com>
2021-09-27 23:12:17 +0600

    H. Bappi

--- app/src/main/java/com/efortshub/holyquran/activities/MainActivity.java
+++ app/src/main/java/com/efortshub/holyquran/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package com.efortshub.holyquran.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+       // AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main);
 
 
