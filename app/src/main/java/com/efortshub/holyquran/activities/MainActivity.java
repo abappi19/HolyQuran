60ebc94fea343128311e858123bff3158dd9905b
hbappi <contact.efortshub@gmail.com>
2021-09-27 12:56:20 +0600

    H. Bappi

--- app/src/main/java/com/efortshub/holyquran/activities/MainActivity.java
+++ app/src/main/java/com/efortshub/holyquran/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.efortshub.holyquran;
+package com.efortshub.holyquran.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.efortshub.holyquran.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/efortshub/holyquran/utils/HbConst.java
@@ -0,0 +1,16 @@
+package com.efortshub.holyquran.utils;
+
+/**
+ * Created by H. Bappi on  12:52 PM 9/27/21.
+ * Contact email:
+ * contact@efortshub.com
+ * bappi@efortshub.com
+ * contact.efortshub@gmail.com
+ * Copyright (c) 2021 eFortsHub . All rights reserved.
+ **/
+public class HbConst {
+
+    public static final String BASE_URL = "http://192.168.27.109/efortshub/apis/quran/";
+
+
+}
